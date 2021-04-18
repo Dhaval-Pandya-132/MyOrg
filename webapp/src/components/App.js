a45622277f8db6daa7e99426a00a9fe12865b6b6
Dhaval Pandya <pandya.d@northeastern.edu>
2021-04-17 22:49:51 -0400

    update the css for signup page

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -5,9 +5,10 @@ import Login from './Login/login';
 
 class App extends React.Component {
 
-  render(){
-    return(
-      <div>
+  render() {
+    return (
+      <div className="root-container">
+        <h1 >My Organization</h1>
         <Login {...this.props}></Login>
       </div>
     );
